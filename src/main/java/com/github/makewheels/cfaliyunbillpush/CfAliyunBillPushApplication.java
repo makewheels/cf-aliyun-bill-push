4471022c4d742b7959f9ae40e6f6455859592d31
makewheels <33256842+makewheels@users.noreply.github.com>
2022-06-24 22:35:36 +0800

    init

---
+++ src/main/java/com/github/makewheels/cfaliyunbillpush/CfAliyunBillPushApplication.java
@@ -0,0 +1,13 @@
+package com.github.makewheels.cfaliyunbillpush;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CfAliyunBillPushApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CfAliyunBillPushApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/github/makewheels/cfaliyunbillpush/CfAliyunBillPushApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.makewheels.cfaliyunbillpush;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CfAliyunBillPushApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
