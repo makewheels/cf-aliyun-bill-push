8768f30e8e6ab33778868192d3c5b277668ec472
makewheels <33256842+makewheels@users.noreply.github.com>
2022-06-25 11:50:13 +0800

    邮件推送

--- src/main/java/com/github/makewheels/cfaliyunbillpush/CloudFunction.java
+++ src/main/java/com/github/makewheels/cfaliyunbillpush/CloudFunction.java
@@ -1,8 +1,10 @@
 package com.github.makewheels.cfaliyunbillpush;
 
-import cn.hutool.core.codec.Base64;
 import cn.hutool.core.date.DateUtil;
+import cn.hutool.core.util.IdUtil;
+import cn.hutool.http.HttpUtil;
 import com.alibaba.fastjson2.JSON;
+import com.alibaba.fastjson2.JSONObject;
 import com.aliyun.bssopenapi20171214.Client;
 import com.aliyun.bssopenapi20171214.models.QueryAccountTransactionsRequest;
 import com.aliyun.bssopenapi20171214.models.QueryAccountTransactionsResponseBody;
@@ -16,6 +18,7 @@ import java.io.OutputStream;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 import java.util.*;
+
 public class CloudFunction implements StreamRequestHandler {
     private Client client;
 
@@ -115,6 +118,22 @@ public class CloudFunction implements StreamRequestHandler {
         for (String key : keySet) {
             to.merge(key, from.get(key), Integer::sum);
         }
+    }
+
+    private void sendEmail(List<DailyCost> allDayCosts, Map<String, Integer> week, Map<String, Integer> month) {
+        //组装发送邮件参数
+        JSONObject body = new JSONObject();
+        body.put("toAddress", "finalbird@foxmail.com");
+        body.put("fromAlias", "push-center");
+        body.put("subject", "阿里云消费");
+        body.put("htmlBody", "昨天：" + allDayCosts.get(0).getMap()
+                + JSON.toJSONString(week) + "<br>"
+                + JSON.toJSONString(month));
+        //调用推送中心
+        String response = HttpUtil.post(
+                "http://push-center.java8.icu:5025/push/sendEmail",
+                body.toJSONString());
+        System.out.println(response);
     }
 
     @Override
